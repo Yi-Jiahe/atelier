7626de4f482ea87b285344e85421c3af66d6d578
Jiahe <yi.jiahe00@gmail.com>
2025-05-28 22:01:51 +0800

    Resize grid

--- app/page.tsx
+++ app/page.tsx
@@ -20,12 +20,12 @@ export default function Home() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div className="grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-[8px]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start w-full">
+        <div className="grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-[8px] w-full">
           {thumbnails.map((thumbnail: ArtworkThumbnail, index: number) => {
             return (
               <a key={index} href={`artwork/${thumbnail.artworkKey}`} rel="noopener noreferrer">
-                <img src={`${thumbnail.href}?width=200`} alt={thumbnail.alt} style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+                <img src={`${thumbnail.href}?width=400&height=400`} alt={thumbnail.alt} style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
               </a>
             );
           })}
